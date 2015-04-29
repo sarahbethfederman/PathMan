5b28e2d768eb60bd920fcd9199a6eee6c5c149c6
jal5313 <jal5313@rit.edu>
2015-04-29 16:43:29 -0400

    Added an exampleGrid.py file that can be executed to show an example grid easily.

---
+++ src/exampleGrid.py
@@ -0,0 +1,13 @@
+from pattern import *
+from grid import *
+
+p = Pattern()
+p.add_operation('+1')
+p.add_operation('+5')
+p.add_operation('+4')
+
+g = Grid(10, 10, 0, 123, p)
+g.generate_grid()
+g.print_grid()
+
+p.humanize()
