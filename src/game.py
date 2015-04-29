4e2f0afc0dd2867cc95462ea2b94c292052cc025
Ryan Gannon <ryanmgannon@gmail.com>
2015-04-29 17:51:40 -0400

    Added boilerplate for game class

---
+++ src/game.py
@@ -0,0 +1,11 @@
+# Game: an over-arching class to encapsulate the grid, player, and time.
+# Chunks are missing which correspond to Player code that doesn't exist yet.
+# Author: Ryan Gannon (IAmRasputin)
+
+from grid import *
+from pattern import *
+
+class Game():
+    def __init__():
+        # Nothing is automatic here.
+        pass
