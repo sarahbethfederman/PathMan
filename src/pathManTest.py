3ea046cef5972295622eeb13a588f5ce2d319f8b
jal5313 <jal5313@rit.edu>
2015-05-04 16:49:12 -0400

    updated pathManTest.py to have a standard grid. Making exampleGrid.py obsolete. Deleted exampleGrid.py

--- src/pathManTest.py
+++ src/pathManTest.py
@@ -6,7 +6,7 @@ adding_Operations = True
 adding_Seed = False
 p = Pattern()
 
-print("Please add operations. When done, type 'finished'")
+print("Please add operations. When done, type 'finished' or type 'standard' for a standard grid")
 
 while adding_Operations == True:
     user_input = raw_input("Add an operation: ")
@@ -14,6 +14,14 @@ while adding_Operations == True:
     if user_input == "finished":
         adding_Operations = False
         adding_Seed = True
+    elif user_input == "standard":
+        p = Pattern()
+        p.add_operation('+5')
+        p.add_operation('+1')
+        p.add_operation('+4')
+        g = Grid(10, 10, 0, 2, p)
+        adding_Operations = False
+
     if adding_Operations != False:
         print("Attempting to add: " + user_input)
         p.add_operation(user_input)
